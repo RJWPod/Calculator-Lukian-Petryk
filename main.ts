18aa172f547db3d6cfec582998b5fcca3a36d558
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-23 18:19:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    Number_1 += 1
+    basic.showNumber(Number_1)
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Mode == 0) {
+        basic.showString("+")
+        Number_2 = Number_1
+        Number_1 = 0
+        Mode = 1
+    } else {
+        basic.showString("=")
+        basic.showString("" + (Number_1 + Number_2))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Number_1 += -1
+    basic.showNumber(Number_1)
+})
+let Mode = 0
+let Number_2 = 0
+let Number_1 = 0
+basic.showIcon(IconNames.Yes)
+Number_1 = 0
+Number_2 = 0
+Mode = 0
